d503c29581f0230d382e8d108b7f9fec2b5f6f00
BobrovskaOlena <lena69114@gmail.com>
2023-08-13 20:23:23 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, this calculator adds numbers from 1 to n! Start");
+        SumCalculator sumCalculator=new SumCalculator();
+        sumCalculator.sum(3);
+
+    }
+}
---
+++ src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class SumCalculator {
+    public int sum(int n){
+        if (n <= 0) {
+            throw new IllegalArgumentException("Cannot sum zero values");}
+        int sum = 0;
+        for (int i = 1; i <= n; i++){ sum += i;}
+        return sum;
+    }
+}
