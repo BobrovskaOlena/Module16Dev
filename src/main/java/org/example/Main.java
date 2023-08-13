d91b68ff9323fd10d6adba63076c9f52f3c18701
BobrovskaOlena <lena69114@gmail.com>
2023-08-13 20:38:57 +0300

    update message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, this calculator adds numbers from 1 to n! Start");
+        System.out.println("Hello, this calculator adds numbers from 1 to n! Go to counting");
         SumCalculator sumCalculator=new SumCalculator();
         sumCalculator.sum(3);
 
